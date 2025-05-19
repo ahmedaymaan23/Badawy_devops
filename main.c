692693a7d22f0fe08e7dd505261d33b1638768ed
AHMED-AYMAN\Ahmed Ayman <ahmedaymaan2025.gmail.com>
2025-05-19 14:36:01 +0300

    Revert "fix ssh"
    This reverts commit 8882fe99e26de42f0fa08e3e6aa4b59058b67bab.

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
-:
+// main.c
+#include <stdio.h>
+
+int main() {
+    // Print a welcome message
+    printf("Hello, devops team, I'm here!\n");
+
+    // Declare two integers
+    int a, b, sum;
+
+    // Ask user for input
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    // Calculate their sum
+    sum = a + b;
+
+    // Print the result
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+
+    return 0;
+}
